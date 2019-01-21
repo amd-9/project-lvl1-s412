18485848b66845a330255a6077b94fa5c8fd37b1
Anton Samoshin <asamoshin9@gmail.com>
2019-01-21 14:06:55 +0300

    Fix project structure

--- src/game-engine.js
+++ src/game-engine.js
@@ -1,4 +1,3 @@
-
 import readlineSync from 'readline-sync';
 
 const maxWinCount = 3;
@@ -9,15 +8,14 @@ const runGame = (stage, description) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
 
-  for (let winCount = 0; winCount < maxWinCount;) {
-    const stageParameters = stage();
-    console.log(`Question: ${stageParameters.question}`);
+  for (let winCount = 0; winCount < maxWinCount; winCount += 1) {
+    const [question, answer] = stage();
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === stageParameters.answer) {
-      winCount += 1;
+    if (userAnswer === answer) {
       console.log('Correct!');
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${stageParameters.answer}'.`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
